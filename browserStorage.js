d600ad4d23444b2ec1f8c82a077397bd282d63b8
Michael Strutt <michael@mstrutt.co.uk>
2014-05-11 16:20:10 +0100

    persisting to browser and namespaced

--- browserStorage.js
+++ browserStorage.js
@@ -1,22 +1,30 @@
-(function(){
+(function(namespace){
 	var browserStorage = function(){
-		var data = {};
+		var data = false,
+			init = function() {
+				data = JSON.parse(localStorage.getItem(namespace)) || {};
+			};
 
 		this.get = function (key) {
+			if (!data) init();
 			return data[key];
 		};
 
 		this.set = function (key, val) {
-			if (arguments.length === 2)
+			if (!data) init();
+			if (arguments.length === 2) {
 				data[key] = val;
-			else
-				return false;
+				localStorage.setItem(namespace, JSON.stringify(data));
+			}
+			return arguments.length === 2;
 		};
 
 		this.clear = function (key) {
+			if (!data) init();
 			delete data[key];
+			localStorage.setItem(namespace, JSON.stringify(data));
 		};
 	};
 
-	window.browserStorage = new browserStorage();
-})();
+	window[namespace] = new browserStorage();
+})('browserStorage');
