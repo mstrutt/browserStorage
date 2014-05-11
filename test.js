765281e31c0fabea6f604cf268349991b8d82e2f
Michael Strutt <michael@mstrutt.co.uk>
2014-05-11 17:58:27 +0100

    adding in some basic tests and a debonced form persisting demo

---
+++ test.js
@@ -0,0 +1,54 @@
+function runTests () {
+	console.log("browserStorage.get('something')");
+	console.log(browserStorage.get('something'));
+
+	console.log("localStorage.clear()");
+	localStorage.clear();
+
+	console.log("browserStorage.get('something')");
+	console.log(browserStorage.get('something'));
+
+	console.log("browserStorage.set('something', 'something else')");
+	console.log(browserStorage.set('something', 'something else'));
+
+	console.log("browserStorage.get('something')");
+	console.log(browserStorage.get('something'));
+
+	var myObject = {
+		"one": 1,
+		"two": 2,
+		"three": 3
+	};
+	console.log(myObject);
+	console.log("browserStorage.set('myObject', myObject)");
+	console.log(browserStorage.set('myObject', myObject));
+	console.log("browserStorage.get('myObject').two");
+	console.log(browserStorage.get('myObject').two);
+
+	console.log("browserStorage.set('something', 'this will persist')");
+	console.log(browserStorage.set('something', 'this will persist'));
+
+	return "Tests complete.";
+}
+
+
+(function(){
+	var formData = browserStorage.get('formData') || {},
+		debounce;
+
+	function persistForm (e) {
+		clearTimeout(debounce);
+		formData[this.name] = this.value;
+
+		debounce = setTimeout(function(){
+			browserStorage.set('formData', formData);
+		}, 200);
+	}
+
+	[].forEach.call(document.forms['sample-form'].elements, function(input){
+		input.onkeyup = persistForm;
+
+		if (formData[input.name])
+			input.value = formData[input.name];
+	});
+})();
