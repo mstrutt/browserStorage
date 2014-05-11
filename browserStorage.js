fde02364e20796c77bb622d134c82de2cf4b0357
Michael Strutt <michael@mstrutt.co.uk>
2014-05-11 17:11:00 +0100

    cross-browser

--- browserStorage.js
+++ browserStorage.js
@@ -1,16 +1,26 @@
 (function(namespace){
+	if (Modernizr) {
+		Modernizr.load({
+			test: Modernizr.localstorage,
+			nope: 'polyfill/localStorage.js'
+		},{
+			test: window.JSON,
+			nope: 'polyfill/json.js'
+		});
+	}
+
 	var browserStorage = function(){
 		this.get = function (key) {
-			var val = localStorage[namespace+key];
+			var val = localStorage.getItem(namespace+key);
 			return (val) ? JSON.parse(val) : undefined;
 		};
 
 		this.set = function (key, val) {
-			return (arguments.length === 2) ? localStorage[namespace+key] = JSON.stringify(val) : false;
+			return (arguments.length === 2) ? localStorage.setItem(namespace+key, JSON.stringify(val)) : false;
 		};
 
 		this.clear = function (key) {
-			return delete localStorage[namespace+key];
+			return localStorage.removeItem(namespace+key);
 		};
 	};
 
